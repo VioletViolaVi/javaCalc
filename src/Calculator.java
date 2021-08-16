46e73be99d7029c231c2c30d7aacdbf45529ad89
VioletViola <vivian.v18723@gmail.com>
2021-08-16 22:11:37 +0100

    Add divide and multiply methods.

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,4 +5,10 @@ public class Calculator {
     public int subtractNumbers(int num1, int num2) {
         return num1 - num2;
     }
+    public int multiplyNumbers(int num1, int num2) {
+        return num1 * num2;
+    }
+    public int divideNumbers(int num1, int num2) {
+        return num1 / num2;
+    }
 }
